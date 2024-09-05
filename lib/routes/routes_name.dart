f79202d1a68d4122bd8ecb99e62a5ab422844425
Haider228 <flutter2830@gmail.com>
2024-09-05 23:01:58 +0500

    adding view and routes

---
+++ lib/routes/routes_name.dart
@@ -0,0 +1,5 @@
+class RoutesName{
+  static const String homeScreen = "home_screen" ;
+  static const String splashScreen = "splash_screen" ;
+  static const String paymentScreen = "payment_screen" ;
+}
---
+++ lib/view/splash_screen_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SplashScreenView extends StatefulWidget {
+  const SplashScreenView({super.key});
+
+  @override
+  State<SplashScreenView> createState() => _SplashScreenViewState();
+}
+
+class _SplashScreenViewState extends State<SplashScreenView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
