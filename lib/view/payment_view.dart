9b81900c8b32960c4571a8e97c532d4ab8f85314
Haider228 <flutter2830@gmail.com>
2024-09-21 13:14:02 +0500

    error handling in payment screen

--- lib/view/payment_view.dart
+++ lib/view/payment_view.dart
@@ -17,94 +17,140 @@ class PaymentView extends StatefulWidget {
 class _PaymentViewState extends State<PaymentView> {
   TextEditingController amountController = TextEditingController();
   final FocusNode amountFocusNode = FocusNode();
+
+  // Add a loading state variable
+  bool isLoading = false;
+
+  void handleBottomSheetDismissal() {
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: SafeArea(
-            child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacementNamed(
-                      context, RoutesName.signUpScreen);
-                },
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: AppColor.themeColor
-                        .withOpacity(0.1), // Light shade of themeColor
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(8),
-                  child:
-                      const Icon(Icons.arrow_back, color: AppColor.themeColor),
-                ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.03,
-              ),
-              SmallWidgets.circularIcon(context, Icons.attach_money_outlined),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.02,
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  "Payment",
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "Poppins"),
-                ),
-              ),
-              RichText(
-                text: const TextSpan(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextSpan(
-                        text:
-                            "Join the movement for change. Donate to give hope to ",
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacementNamed(
+                            context, RoutesName.signUpScreen);
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: AppColor.themeColor
+                              .withOpacity(0.1), // Light shade of themeColor
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: const Icon(Icons.arrow_back,
+                            color: AppColor.themeColor),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                    SmallWidgets.circularIcon(
+                        context, Icons.attach_money_outlined),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      child: Text(
+                        "Payment",
                         style: TextStyle(
-                            color: Colors.black, fontFamily: "Poppins")),
-                    TextSpan(
-                        text: " GiveHope",
-                        style: TextStyle(
-                            color: AppColor.themeColor,
+                            fontSize: 30,
                             fontWeight: FontWeight.bold,
-                            fontFamily: "Poppins")),
-                    TextSpan(
-                        text: " members",
-                        style: TextStyle(
-                            color: Colors.black, fontFamily: "Poppins")),
+                            fontFamily: "Poppins"),
+                      ),
+                    ),
+                    RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                              text:
+                                  "Join the movement for change. Donate to give hope to ",
+                              style: TextStyle(
+                                  color: Colors.black, fontFamily: "Poppins")),
+                          TextSpan(
+                              text: " GiveHope",
+                              style: TextStyle(
+                                  color: AppColor.themeColor,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: "Poppins")),
+                          TextSpan(
+                              text: " members",
+                              style: TextStyle(
+                                  color: Colors.black, fontFamily: "Poppins")),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    SmallWidgets.textIs("Enter Amount"),
+                    CustomTextField(
+                        controllerIs: amountController,
+                        hintTextIs: "Rs.3000",
+                        focusNode: amountFocusNode,
+                        keyboardApperanceType: TextInputType.number,
+                        prefixIconIs: Icons.attach_money_rounded),
+                    RoundButton(
+                      buttonText: "Next",
+                      onPressed: () async {
+                        if (amountController.text.isEmpty) {
+                          // Check if the amount is not empty
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text("Please enter an amount")),
+                          );
+                          return;
+                        }
+                        setState(() {
+                          isLoading = true;
+                        });
+                        try {
+                          await StripeService.instance
+                              .makePayment(int.parse(amountController.text));
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(e.toString())),
+                          );
+                        } finally {
+                          setState(() {
+                            isLoading = false;
+                          });
+                        }
+
+                        amountController.clear();
+                      },
+                    ),
                   ],
                 ),
               ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.02,
-              ),
-              SmallWidgets.textIs("Enter Amount"),
-              CustomTextField(
-                  controllerIs: amountController,
-                  hintTextIs: "Rs.3000",
-                  focusNode: amountFocusNode,
-                  keyboardApperanceType: TextInputType.number,
-                  prefixIconIs: Icons.attach_money_rounded),
-              RoundButton(
-                buttonText: "Next",
-                onPressed: () {
-                  StripeService.instance
-                      .makePayment(int.parse(amountController.text));
-                  amountController.clear();
-                },
-              )
-            ],
+            ),
           ),
-        )),
+          // Show CircularProgressIndicator when isLoading is true
+          if (isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
       ),
     );
   }
