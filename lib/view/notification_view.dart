a0a4d011386ed309aec7c3ed4a1e609ab1fc3558
Haider228 <flutter2830@gmail.com>
2024-09-24 11:37:13 +0500

    adding notification screen

---
+++ lib/view/notification_view.dart
@@ -0,0 +1,44 @@
+import 'package:donation_app/utils/app_color.dart';
+import 'package:flutter/material.dart';
+
+class NotificationView extends StatelessWidget {
+  const NotificationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.backgroundBodyColor,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: AppColor.themeColor.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(8),
+                    child: const Icon(Icons.arrow_back,
+                        color: AppColor.themeColor),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
